edbe3837837155b67f3183991652613d296bc194
romelukaku <gundamaster5@gmail.com>
2021-12-13 16:19:44 +0700

    add dig sale accs, total added: 126586010000000

--- genesis.py
+++ genesis.py
@@ -47,11 +47,11 @@ dir = "genesis.json"
 # liquidity_boostrapping = {"dig13qxgtvk3ttjfygm7qk4ftd8yvz63qv5zx6tuq6": 47385737000000}
 # genesis_util.AddAccountsIntoGenesis(dir, liquidity_boostrapping)
 
-validator_treasury = {"dig1880q0w9dzt439fgmwslvaur7pq6jv0fzx4zx0a": 71078605000000}
-genesis_util.AddAccountsIntoGenesis(dir, validator_treasury)
+# validator_treasury = {"dig1880q0w9dzt439fgmwslvaur7pq6jv0fzx4zx0a": 71078605000000}
+# genesis_util.AddAccountsIntoGenesis(dir, validator_treasury)
 
-# sale = dig_sale.GetSaleMap()
-# genesis_util.AddAccountsIntoGenesis("genesis.json", sale)
+sale = dig_sale.GetSaleMap()
+genesis_util.AddAccountsIntoGenesis("genesis.json", sale)
 
 # founder = dig_sale.GetFounders()
 # genesis_util.AddAccountsIntoGenesis(dir, founder)
